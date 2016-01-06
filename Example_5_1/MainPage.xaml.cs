7ab368c0593c2b99dc523fcdd0b78c85555e89da
Boris Breuer <borisbreuer1@gmail.com>
2016-01-06 19:11:40 +0100

    :point_up_2: Added example 1 from module 1 of DEV206.2x: Designing Advanced Applications using XAML.

---
+++ Example_5_1/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Threading.Tasks;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Example_5_1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadData();
+        }
+
+        public static readonly DependencyProperty IsLoadingProperty = DependencyProperty.Register(
+            "IsLoading", typeof(bool), typeof(MainPage), new PropertyMetadata(false)
+            );
+
+        public Boolean IsLoading
+        {
+            get { return (Boolean)GetValue(IsLoadingProperty); }
+            set { SetValue(IsLoadingProperty, value); }
+        }
+
+        async void LoadData()
+        {
+            this.IsLoading = true;
+
+            await Task.Delay(5000);
+
+            this.IsLoading = false;
+        }
+    }
+}

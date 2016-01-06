64d606f014be1f23840e1431c09e108929034763
Boris Breuer <borisbreuer1@gmail.com>
2016-01-06 21:12:50 +0100

    :zap: Added example 2 of module 1 of DEV206.2x: Designing Advanced Applications using XAML.

---
+++ Example_5_2/LoadingProperties.cs
@@ -0,0 +1,22 @@
+﻿using Windows.UI.Xaml;
+
+namespace Example_5_2
+{
+    public class LoadingProperties : DependencyObject
+    {
+        public static readonly DependencyProperty IsLoadingProperty = DependencyProperty.RegisterAttached(
+            "IsLoading", typeof(bool), typeof(LoadingProperties), new PropertyMetadata(true)
+            );
+
+        public static void SetIsLoading(UIElement element, bool value)
+        {
+            element.SetValue(IsLoadingProperty, value);
+        }
+        public static bool GetIsLoading(UIElement element)
+        {
+            return (bool)element.GetValue(IsLoadingProperty);
+        }
+    }
+
+
+}
